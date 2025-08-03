ffdc9853e081cf2dff0677e68299feab563d2482
“ali_sh” <ali_shahsavari1996@yahoo.com>
2025-08-03 16:42:17 +0430

    [add] routes and fontAwsome

--- nak_challenge/src/App.tsx
+++ nak_challenge/src/App.tsx
@@ -1,20 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import SignUpForm from './components/SignUpPage';
 import SignInPage from './components/SignInPage';
+import Dashboard from './components/Dashboard';
+import Attributes from './components/Attribiuttes';
+import Layout from './components/Layout';
 
-// const LoginPage = () => <div>Login Page</div>;
-const DashboardPage = () => <div>Welcome to your Dashboard!</div>;
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/attributes" element={<Attributes />} />
+      </Route>
       <Route path="/login" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpForm />} />
-
-      <Route path="/" element={<DashboardPage />} />
-
       <Route path="*" element={<div>404 - Page Not Found</div>} />
-    </Routes>
+    </Routes >
   );
 }
 
