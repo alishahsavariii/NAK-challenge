8c7e568197d637a3e806b88cac53798f7cacfb43
“ali_sh” <ali_shahsavari1996@yahoo.com>
2025-08-03 16:44:34 +0430

    [feat] handle signIn in hot reload

--- nak_challenge/src/stores/authStore.ts
+++ nak_challenge/src/stores/authStore.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 import { create } from 'zustand';
 
- // eslint-disable-next-line @typescript-eslint/no-unused-vars
- const registerSchema = z.object({
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const registerSchema = z.object({
   firstName: z.string().min(1, { message: "errors.nameRequired" }),
   lastName: z.string().min(1, { message: "errors.lastName" }),
   userName: z.string().min(1, { message: "errors.userName" }),
@@ -13,9 +13,7 @@ import { create } from 'zustand';
   path: ["confirmPassword"],
 });
 
-
 export type RegisterFormData = z.infer<typeof registerSchema>;
-
 type RegisterApiData = Omit<RegisterFormData, "confirmPassword">;
 
 
@@ -29,56 +27,72 @@ export type LoginFormData = z.infer<typeof loginSchema>;
 interface AuthState {
   isLoading: boolean;
   error: string | null;
-  register: (data: RegisterApiData) => Promise<boolean>; 
+  register: (data: RegisterApiData) => Promise<boolean>;
   login: (data: LoginFormData) => Promise<boolean>;
-
+  userName?: string | null;
+  logout: () => void;
+  isLoggedIn: boolean;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  isLoading: false,
-  error: null,
-  register: async (data) => {
-    set({ isLoading: true, error: null });
-    try {
-      const response = await fetch('api/users/register', { 
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-        
-      });
 
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message || 'errors.registrationFailed');
+export const useAuthStore = create<AuthState>((set) => {
+  //localsg
+  const storedUserName = localStorage.getItem('userName');
+  const isLoggedIn = storedUserName !== null;
+
+  return {
+    isLoading: false,
+    error: null,
+    isLoggedIn,
+    userName: storedUserName,
+    register: async (data) => {
+      set({ isLoading: true, error: null });
+      try {
+        const response = await fetch('api/users/register', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data),
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.message || 'errors.registrationFailed');
+        }
+
+        set({ isLoading: false });
+        return true;
+
+      } catch (err) {
+        set({ isLoading: false, error: err.message });
+        return false;
       }
+    },
+    login: async (data) => {
+      set({ isLoading: true, error: null });
+      try {
+        const response = await fetch('api/auth/login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data),
+        });
 
-      set({ isLoading: false });
-      return true;
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.message || 'errors.loginFailed');
+        }
 
-    } catch (err) {
-      set({ isLoading: false, error: err.message });
-      return false;
-    }
-  },
-  login: async (data) => {
-    set({ isLoading: true, error: null });
-    try {
-      const response = await fetch('api/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-      });
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message || 'errors.loginFailed');
+        // add to localsg
+        localStorage.setItem('userName', data.userName);
+        set({ isLoading: false, userName: data.userName, isLoggedIn: true });
+        return true;
+      } catch (err) {
+        set({ isLoading: false, error: err.message });
+        return false;
       }
-      set({ isLoading: false });
-      return true;
-    } catch (err) {
-      set({ isLoading: false, error: err.message });
-      return false;
-    }
-  },
-}));
-
-
+    },
+    logout: () => {
+      localStorage.removeItem('userName');
+      set({ userName: null, isLoggedIn: false });
+    },
+  };
+});
